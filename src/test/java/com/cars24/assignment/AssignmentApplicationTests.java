ed580e0ff56e6d319fa66c7245aba3c0486ec876
Pawan Kumar <v-pkumar6@expedia.com>
2020-09-23 17:01:40 +0530

    initial commit of code

---
+++ src/test/java/com/cars24/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars24.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
